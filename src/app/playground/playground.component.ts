90fb529a13bc608c511e0430fa85a546a41da44f
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-09 21:39:36 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  AfterContentInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent implements OnInit, AfterViewInit {
+export class PlaygroundComponent
+  implements OnInit, AfterViewInit, AfterContentInit {
   label = 'PlaygroundComponent';
 
   constructor() {
+    //  One-time notification
     console.log(`${this.label}: constructor`);
   }
 
@@ -16,11 +23,21 @@ export class PlaygroundComponent implements OnInit, AfterViewInit {
   //  explicitly implementing the interfaces
   //  But it is a good practice to implement the interfaces
   ngOnInit() {
+    //  One-time notification
     //  Template rendering starts
     console.log(`${this.label}: ngOnInit`);
   }
 
+  ngAfterContentInit(): void {
+    //  One-time notification
+    //  Called after projected content rendering ends
+    //  Order: after ngOnInit, before ngAfterViewInit
+    //  This will be called regardless of content-projection
+    console.log(`${this.label}: ngAfterContentInit`);
+  }
+
   ngAfterViewInit(): void {
+    //  One-time notification
     //  Template rendering ends
     console.log(`${this.label}: ngAfterViewInit`);
   }
@@ -29,4 +46,5 @@ export class PlaygroundComponent implements OnInit, AfterViewInit {
 //  So far
 //  1.  constructor
 //  2.  ngOnInit
-//  3.  ngAfterViewInit
+//  3.  ngAfterContentInit
+//  4.  ngAfterViewInit
