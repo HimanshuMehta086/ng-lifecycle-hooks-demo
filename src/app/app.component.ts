2da653f47d2bd75de263180a99d49fb8111c4490
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-09 21:23:59 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   label = 'AppComponent';
 
-  constructor() {}
+  constructor() {
+    console.log(`${this.label}: constructor`);
+  }
+
+  ngOnInit(): void {
+    //  Starts after child constructor, before child ngOnInit
+    console.log(`${this.label}: ngOnInit`);
+  }
+
+  ngAfterViewInit(): void {
+    //  Ends after all children have been rendered (after all child ngAfterViewInit)
+    console.log(`${this.label}: ngAfterViewInit`);
+  }
 }
