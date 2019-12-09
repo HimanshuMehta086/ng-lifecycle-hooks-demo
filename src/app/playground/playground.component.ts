686d82b14283b27bfe9ab0f4cb222acd17d34520
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-09 21:19:50 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent implements OnInit {
+export class PlaygroundComponent implements OnInit, AfterViewInit {
   label = 'PlaygroundComponent';
 
   constructor() {
     console.log(`${this.label}: constructor`);
   }
 
-  ngOnInit() {}
+  //  It is possible to write the lifecycle methods without
+  //  explicitly implementing the interfaces
+  //  But it is a good practice to implement the interfaces
+  ngOnInit() {
+    //  Template rendering starts
+    console.log(`${this.label}: ngOnInit`);
+  }
+
+  ngAfterViewInit(): void {
+    //  Template rendering ends
+    console.log(`${this.label}: ngAfterViewInit`);
+  }
 }
